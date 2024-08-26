8c7ab27723cd952ae739e5393e205acfd4443b40
Denys <dum.denya15@gmail.com>
2024-08-26 21:46:36 +0300

    fix issue with multiple requests for API

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -8,12 +8,10 @@ import router from 'routes/router';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
     <ThemeProvider theme={theme}>
       <BreakpointsProvider>
         <CssBaseline />
         <RouterProvider router={router} />
       </BreakpointsProvider>
     </ThemeProvider>
-  </React.StrictMode>,
 );
